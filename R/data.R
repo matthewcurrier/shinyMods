45b3e0c8212e034c1e8fc31e6656557c55003439
matthewcurrier <currier.matthew@gmail.com>
2024-11-15 17:40:11 -0800

    fixed description file and removed unused datasets

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' A sales dataset suggested in the Mastering Shiny book
+#'
+#'
+#' @format ## `sales`
+#' A data frame with 2,823 rows and 25 columns
+#' @source <https://www.kaggle.com/datasets/kyanyoga/sample-sales-data>
+"sales"
