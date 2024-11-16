cdbef8325943927215520bdf3f1faff2d2ea6e5b
matthewcurrier <currier.matthew@gmail.com>
2024-11-16 09:49:08 -0800

    corrected documentation error for covid dataset

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #' A data frame with 2,823 rows and 25 columns
 #' @source <https://www.kaggle.com/datasets/kyanyoga/sample-sales-data>
 "sales"
-#' A sales dataset suggested in the Mastering Shiny book
+#' A covid dataset from Kaggle, used to test point mapping with the leaflet package
 #'
 #'
 #' @format ## `covid`
